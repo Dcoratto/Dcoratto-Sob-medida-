f87cf9c6738219f215fa3afff0a1b0a1f60bf0a1
briantakiya77 <dcorattoinovacao@gmail.com>
2026-05-22 13:06:27 -0300

    TESTE FINAL 03

--- vite.config.mjs
+++ vite.config.mjs
@@ -20,7 +20,7 @@ export default defineConfig(({mode}) => {
     },
     optimizeDeps: {
       noDiscovery: true,
-      include: ['react-router-dom', 'react-router', 'cookie'],
+      include: ['react', 'react-dom', 'react-dom/client', 'react-router-dom', 'react-router', 'cookie'],
     },
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
